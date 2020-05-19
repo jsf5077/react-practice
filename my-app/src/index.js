830c650c21f3cb96da1bfcd9f0e35a95dd8e7633
jsf5077 <jsf5077@gmail.com>
2020-05-18 23:16:21 -0400

    Clean Slate

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,8 @@
-//this is the js file that links all of the components with the html.
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import './index.css';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "./index.css";
-
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +11,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
