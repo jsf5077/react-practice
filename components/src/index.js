ca342bd35cec7b7afe26f12c0defa0b90c8078ad
jsf5077 <jsf5077@gmail.com>
2020-05-29 00:25:08 -0400

    Continued projects working with props and prop children

--- components/src/index.js
+++ components/src/index.js
@@ -1,29 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 import faker from "faker";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="sam"
-        timeAgo="Today at 4:45PM"
-        text="I rock!"
-        image={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jeff"
-        timeAgo="Today at 1:00AM"
-        text="I found the meal to be a bit pedantic"
-        image={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Liz"
-        timeAgo="Tuesday at 5:30PM"
-        text="COVID-19 has ruined my life!"
-        image={faker.image.avatar()}
-      />
+      <ApprovalCard>Still a good move?</ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="sam"
+          timeAgo="Today at 4:45PM"
+          text="I rock!"
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jeff"
+          timeAgo="Today at 1:00AM"
+          text="I found the meal to be a bit pedantic"
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Liz"
+          timeAgo="Tuesday at 5:30PM"
+          text="COVID-19 has ruined my life!"
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
